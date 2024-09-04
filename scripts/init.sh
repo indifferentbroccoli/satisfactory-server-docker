e054d344e398db5e2fad0d246c48c1d7391729e9
Thijs van Loef <thijsvanloef@gmail.com>
2024-09-04 12:08:48 +0000

    re-add shutdown

--- scripts/init.sh
+++ scripts/init.sh
@@ -24,5 +24,18 @@ fi
 
 install
 
+# shellcheck disable=SC2317
+term_handler() {
+    LogWarn "SIGTERM received, shutting down server"
+    kill -SIGTERM "$(pidof UnrealServer-Linux-Shipping)"
+    tail --pid="$killpid" -f 2>/dev/null
+}
+
+trap 'term_handler' SIGTERM
+
 # Start the server
-./start.sh
+./start.sh &
+
+# Process ID of su
+killpid="$!"
+wait "$killpid"
